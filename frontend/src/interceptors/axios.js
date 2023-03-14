26dddb98b17b731e0212de012334cba571cb71c9
drutkoowski <d.rutkowski2000@gmail.com>
2023-03-14 01:26:47 +0100

    setting up axios and tailwind

---
+++ frontend/src/interceptors/axios.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom'
+
+
+axios.defaults.baseURL = import.meta.env.VITE_DEVELOPMENT_API_URL;
+
+const AxiosInterceptor = ({ children }) => {
+
+    // const navigate = useNavigate();
+
+    useEffect(() => {
+
+        const resInterceptor = response => {
+            return response;
+        }
+
+        const errInterceptor = error => {
+
+            if (error.response.status === 401) {
+                // navigate('/login');
+            }
+
+            return Promise.reject(error);
+        }
+
+
+        const interceptor = axios.interceptors.response.use(resInterceptor, errInterceptor);
+
+        return () => axios.interceptors.response.eject(interceptor);
+
+    // }, [navigate])
+     }, [])
+    return children;
+}
+export { AxiosInterceptor }
