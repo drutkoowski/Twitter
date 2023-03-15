8b16db88d68115a3d546eb29f44bd7f8baca2536
drutkoowski <d.rutkowski2000@gmail.com>
2023-03-15 15:21:47 +0100

    final touches for unauthorized page

--- frontend/src/components/ads/JoinUs.jsx
+++ frontend/src/components/ads/JoinUs.jsx
@@ -1,6 +1,16 @@
+import Button from '../Button.jsx'
+import '@/styles/components/ads/join_us.scss'
+
 function JoinUs () {
   return (
-        <div>Join us</div>
+        <div className='join-us'>
+            <h4>New to Twitter?</h4>
+            <small>Sign up now to get your own personalized timeline!</small>
+            <Button text='Zarejestruj się przez Google' classNames='white lg'/>
+            <Button text='Zarejestruj się przez Apple' classNames='white lg'/>
+            <Button text='Utwórz konto' classNames='white lg'/>
+            <small>By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.</small>
+        </div>
   )
 }
 
