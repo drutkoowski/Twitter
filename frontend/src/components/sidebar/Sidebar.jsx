cb65ffbeadb8cf5bd337557a0b3d5a0fc39bc39c
drutkoowski <d.rutkowski2000@gmail.com>
2023-03-16 11:58:05 +0100

    fixed sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -8,29 +8,33 @@ function Sidebar () {
   const { user, setUser, isAuthenticated, setIsAuthenticated } = useContext(UserContext)
   console.log(isAuthenticated)
   return (
-        <div className='sidebar'>
-            <div className='sidebar__element'>
-                <img src="/twitter-gray.svg" alt="Twitter logo"/>
-            </div>
-            {!isAuthenticated &&
-                <>
-                    <SidebarLink text='Explore' imgPath='/hash-gray.svg' altText='Explore'/>
-                    <SidebarLink text='Settings' imgPath='/cog-gray.svg' altText='Settings'/>
-                </>
-            }
-            {isAuthenticated &&
-                <>
-                    <SidebarLink text='Home' imgPath='/home.svg' altText='Home'/>
-                    <SidebarLink text='Explore' imgPath='/hash-gray.svg' altText='Explore'/>
-                    <SidebarLink text='Notifications' imgPath='/notification-gray.svg' altText='Notifications'/>
-                    <SidebarLink text='Messages' imgPath='/message-gray.svg' altText='Messages'/>
-                    <SidebarLink text='Bookmarks' imgPath='/bookmarks-gray.svg' altText='Bookmarks'/>
-                    <SidebarLink text='Profile' imgPath='/profile-gray.svg' altText='Profile'/>
-                    <SidebarLink text='More' imgPath='/options-gray.svg' altText='More'/>
-                </>
-            }
 
+        <div className='sidebar'>
+            <div className='sidebar__wrapper'>
+                <div className='sidebar__wrapper__fixed'>
+                    <div className='sidebar__element'>
+                        <img src="/twitter-gray.svg" alt="Twitter logo"/>
+                    </div>
+                    {!isAuthenticated &&
+                        <>
+                            <SidebarLink text='Explore' imgPath='/hash-gray.svg' altText='Explore'/>
+                            <SidebarLink text='Settings' imgPath='/cog-gray.svg' altText='Settings'/>
+                        </>
+                    }
+                    {isAuthenticated &&
+                        <>
+                            <SidebarLink text='Home' imgPath='/home.svg' altText='Home'/>
+                            <SidebarLink text='Explore' imgPath='/hash-gray.svg' altText='Explore'/>
+                            <SidebarLink text='Notifications' imgPath='/notification-gray.svg' altText='Notifications'/>
+                            <SidebarLink text='Messages' imgPath='/message-gray.svg' altText='Messages'/>
+                            <SidebarLink text='Bookmarks' imgPath='/bookmarks-gray.svg' altText='Bookmarks'/>
+                            <SidebarLink text='Profile' imgPath='/profile-gray.svg' altText='Profile'/>
+                            <SidebarLink text='More' imgPath='/options-gray.svg' altText='More'/>
+                        </>
+                    }</div>
+            </div>
         </div>
+
   )
 }
 
